68b058b71c3a651a8e92875c34d4e8a3cd899b6f
Andrea030105 <tummoloandrea05@gmail.com>
2026-02-13 17:40:26 +0100

    fix-passage-with-$post

--- server.php
+++ server.php
@@ -4,14 +4,19 @@ $string = file_get_contents('array.json');
 $array = json_decode($string, true);
 
 if (isset($_POST['language'])) {
-    $item = $_POST['language'];
+
+    /*  $item = $_POST['language'];
 
     $toDoItems = [
         'language' => $item,
         'done' => false
     ];
 
-    $array[] = $toDoItems;
+    $array[] = $toDoItems; */
+
+    /* PASSO DIRETTAMENTE $POST ESSENDO UN OGGETTO MA BISOGNA FARE ATTENZIONE A DARE UN NOME(KEY) ALLE PROIETA' */
+
+    $array[] = $_POST;
 
     file_put_contents('array.json', json_encode($array));
 }
